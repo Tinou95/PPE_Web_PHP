13aa82faa30ac0718523a1f686c8980443ad0167
Quentin <72099011+Tinou95@users.noreply.github.com>
2022-05-08 14:20:05 +0200

    Add files via upload

--- PPE_PK/page1.php
+++ PPE_PK/page1.php
@@ -42,6 +42,7 @@
     <header>
         <img class="image_header" src="./asset/logo_sport.png" alt="sport_plus">
         <h1>Maison des ligues tous les sports  </h1>
+        <a href="./page_login.php" class="redirection">Se connecter</a>
         <span class="image_header2 image_sunny"></span>
     </header>
     <main>
--- PPE_PK/page2.php
+++ PPE_PK/page2.php
@@ -39,6 +39,7 @@
     <header>
         <a href="page1.php"><img class="image_header" src="./asset/logo_sport.png" alt="sport+"></a>
         <h1>Maison des ligues tous les sports</h1>
+        <a href="./page_login.php" class="redirection">Se connecter</a>
         <span class="image_header2 image_sunny"></span>
     </header>
     <main>
